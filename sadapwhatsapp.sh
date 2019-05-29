2ce7756333a3a0781e7923850240d962162a74b0
XniceCraft <49149283+XniceCraft@users.noreply.github.com>
2019-05-29 16:11:25 +0700

    Add files via upload

---
+++ sadapwhatsapp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo Sedang Meloading
+sleep 1
+toilet -f small --gay Hack WA
+echo "1.Sadap WA dengan Nomor HP"
+echo "2.Sadap WA Dengan Phising"
+read tipu
+
+if [ $tipu = 1 ] || [ $tipu = 1 ]
+then 
+sleep 1
+echo "Lu Kena Tipu Wkkwkwkkwk Sabar Bosque"
+fi
+exit
+
+if [ $tipu = 2 ] || [ $tipu = 2 ]
+then
+echo "Mau Aja lu ditipu wkkwkwkwkwk"
+fi 
+exit
